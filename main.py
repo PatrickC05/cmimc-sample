2f6dcd9affae2c738a344f633fd93a915856d8ad
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-01-28 17:49:35 -0500

    ai code written

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-file = 'mmsd3.in'
-out = 'mmsd3.out'
+file = 'mmsd4.in'
+out = 'mmsd4.out'
+import numpy as np
 from dijkstar import Graph, find_path
+def getSum(n):
+
+    sum = 0
+    while (n != 0):
+
+        sum = sum + (n % 10)
+        n = n//10
+
+    return sum
 def get_min(K):
     if K == 1:
         return 1
@@ -12,7 +22,7 @@ def get_min(K):
     nodes = path.nodes
     costs = path.costs
     num = 1
-    print(done)
+    print('done')
     for i in costs:
         if i == 0:
             num *=10
@@ -24,4 +34,5 @@ with open(file, 'r') as f:
     with open(out, 'w') as o:
         for line in f:
             print(line)
-            o.write(str(get_min(int(line)))+'\n')
+            # o.write(str(get_min(int(line)))+'\n')
+            o.write(str(np.argmin([getSum(int(line)*i) for i in range(1,100000000)])+1)+'\n')
